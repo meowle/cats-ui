c4f0b37d67235b97849cfaa82168db80d22b4033
Aseeva Nastya <aaseeva@alfabank.ru>
2018-09-09 18:29:38 +0300

    feat: parse needle

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.post('/search', function (req, res) {
+  const needle = req.body.needle;
+  res.send(`got ${needle}`);
 });
 
 app.listen(3000, function () {
