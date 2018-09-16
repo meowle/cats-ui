3db6ffc32fc403c4887555280d2caa253b2a2e15
Viet Nguyen <0x06065a@gmail.com>
2018-09-17 00:22:12 +0300

    test: setup nightwatch

---
+++ __tests__/e2e/specs/ui-test.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "Demo test Google": function(browser) {
+    browser
+      .url("http://www.google.com")
+      .waitForElementVisible("body", 1000)
+      .setValue("input[type=text]", "nightwatch")
+      .waitForElementVisible("input[name=btnK]", 1000)
+      .end();
+  }
+};
