6ba7d97c62564610d7f8b5f9c316d794153bd578
Artem Bobrov <a.bobrov@tinkoff.ru>
2020-02-04 18:39:39 +0300

    Добавление первоначальной верски главной страницы

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import Test from './pages/main/main'
+import MainPage from './pages/main/main'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>Meowle</h2>
-        <Router>
-          <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/test">Test</Link>
-                </li>
-              </ul>
-            </nav>
-
-            {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-            <Switch>
-              <Route path="/test">
-                <Test />
-              </Route>
-              <Route path="/">main</Route>
-            </Switch>
-          </div>
-        </Router>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/test">test</Route>
+          <Route path="/">
+            <MainPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,10 +1,42 @@
 import React from 'react'
-import { CatsApi } from '../../api/cats'
+import './main.css'
 
 function App() {
-  const text = CatsApi.getAll()
+  // const text = CatsApi.getAll()
 
-  return <div>Hello, {text}</div>
+  // TODO добавить стили (отсутпы, размер шрифта)
+  return (
+    <section className="section full-size">
+      <div className="container full-size">
+        <div className="columns is-mobile is-vcentered full-size">
+          <div className="column is-8 is-offset-2">
+            <div className="columns">
+              <div className="column">
+                <h1>meowle</h1>
+              </div>
+              <div className="column is-hidden-mobile">
+                <figure className="image is-128x128 is-pulled-right">
+                  <img src="/img/cat.png" />
+                </figure>
+              </div>
+            </div>
+            <form>
+              <div className="field">
+                <div className="control">
+                  <input
+                    type="text"
+                    className="input"
+                    placeholder="Введите часть имени"
+                    autocomplete="off"
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default App
