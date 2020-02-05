c78c7c7e78e0a272cdfb531d8a46527a050fb22d
Artem Bobrov <a.bobrov@tinkoff.ru>
2020-02-05 20:15:23 +0300

    Переход на стрину поиска при поиске

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import MainPage from './pages/main/main'
+import { Router, Switch, Route } from 'react-router-dom'
+import { MainPage } from './pages/main/main'
+import history from './utils/history'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router history={history}>
         <Switch>
           <Route path="/test">test</Route>
-          <Route path="/">
+          <Route path="/" exact={true}>
             <MainPage />
           </Route>
+          <Route path="/search/:query">Search page</Route>
         </Switch>
       </Router>
     </div>
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,42 +1,62 @@
 import React from 'react'
+import history from '../../utils/history'
 import './main.css'
 
-function App() {
-  // const text = CatsApi.getAll()
+export class MainPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchName: '',
+      some: 2,
+    }
+  }
+
+  onKeyUp(event) {
+    this.setState({ searchName: event.target.value })
+
+    if (event.key === 'Enter') {
+      this.search()
+    }
+  }
+
+  search() {
+    history.push(`/search/${this.state.searchName}`)
+  }
 
   // TODO добавить стили (отсутпы, размер шрифта)
-  return (
-    <section className="section full-size">
-      <div className="container full-size">
-        <div className="columns is-mobile is-vcentered full-size">
-          <div className="column is-8 is-offset-2">
-            <div className="columns">
-              <div className="column">
-                <h1>meowle</h1>
-              </div>
-              <div className="column is-hidden-mobile">
-                <figure className="image is-128x128 is-pulled-right">
-                  <img src="/img/cat.png" />
-                </figure>
-              </div>
-            </div>
-            <form>
-              <div className="field">
-                <div className="control">
-                  <input
-                    type="text"
-                    className="input"
-                    placeholder="Введите часть имени"
-                    autocomplete="off"
-                  />
+  render() {
+    return (
+      <section className="section full-size">
+        <div className="container full-size">
+          <div className="columns is-mobile is-vcentered full-size">
+            <div className="column is-8 is-offset-2">
+              <div className="columns">
+                <div className="column">
+                  <h1>meowle</h1>
+                </div>
+                <div className="column is-hidden-mobile">
+                  <figure className="image is-128x128 is-pulled-right">
+                    <img src="/img/cat.png" />
+                  </figure>
                 </div>
               </div>
-            </form>
+              <div>
+                <div className="field">
+                  <div className="control">
+                    <input
+                      type="text"
+                      className="input"
+                      placeholder="Введите часть имени"
+                      autoComplete="off"
+                      onKeyUp={this.onKeyUp.bind(this)}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  )
+      </section>
+    )
+  }
 }
-
-export default App
