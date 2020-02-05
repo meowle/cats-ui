8937776db922455c6b3a478e95b1dad3fcd54a3e
Artem Bobrov <a.bobrov@tinkoff.ru>
2020-02-05 18:33:12 +0300

    Добавление методов сервиса cats

--- src/api/cats.js
+++ src/api/cats.js
@@ -3,17 +3,33 @@ import { urls } from '../config'
 
 export class CatsApi {
   /**
+   * Создание нового имени
+   * http://meowle.testops.ru:3001/api-docs-ui/#/default/post_cats_add
+   * @param {!Object[]} cats Список с информацией котов
+   * @param {!string} cats[].name Имя
+   * @param {!string} cats[].gender Пол
+   * @param {?string=} cats[].description Описание
+   * @returns {Promise<Cat>} Промис с объектом кота
+   */
+  static add(cats) {
+    return axios.post(`${urls.catsApi}/cats/add`, {
+      cats,
+    })
+  }
+
+  /**
    * Поиск кота по части имени и полу
    * http://meowle.testops.ru:3001/api-docs-ui/#/default/post_cats_search
    * @param {!string} name Часть имени
    * @param {?string=} gender Пол
    * @returns {Promise<Groups>} Промис с группировкой имен котов
    * @returns
+   * @returns {Object<>}
    */
   static search(name, gender) {
     return axios.post(`${urls.catsApi}/cats/search`, {
       name,
-       gender,
+      gender,
     })
   }
 
@@ -26,21 +42,6 @@ export class CatsApi {
   static getById(id) {
     return axios.get(`${urls.catsApi}/cats/get-by-id`, {
       params: { id },
-    })
-  }
-
-  /**
-   * Создание нового имени
-   * http://meowle.testops.ru:3001/api-docs-ui/#/default/post_cats_add
-   * @param {!Object[]} cats Список с информацией котов
-   * @param {!string} cats[].name Имя
-   * @param {!string} cats[].gender Пол
-   * @param {?string=} cats[].description Описание
-   * @returns {Promise<Cat>} Промис с объектом кота
-   */
-  static add(cats) {
-    return axios.post(`${urls.catsApi}/cats/add`, {
-      cats,
     })
   }
 
@@ -62,5 +63,32 @@ export class CatsApi {
    */
   static getValidations() {
     return axios.get(`${urls.catsApi}/cats/validation`)
+  }
+
+  /**
+   * Сохранение описания имени кота
+   * http://meowle.testops.ru:3001/api-docs-ui/#/default/post_cats_save_description
+   * @param {!number} id ID кота
+   * @param {!string} description Описание имени
+   * @returns {Promise<Cat>} Промис с объектом кота
+   */
+  static saveDescription(id, description) {
+    return axios.post(`${urls.catsApi}/cats/save-description`, {
+      id,
+      catDescription: description,
+    })
+  }
+
+  /**
+   * Получение списка всех имен котов
+   * http://meowle.testops.ru:3001/api-docs-ui/#/default/get_cats_all
+   * @param {!string} order Сортировка (asc | desc)
+   * @param {string=} gender Фильтр по полу
+   */
+  static getAll(order, gender) {
+    return axios.get(`${urls.catsApi}/cats/all`, {
+      order,
+      gender,
+    })
   }
 }
