6602d85a413e6797f462b22e00568adb24a4ecc1
Artem Bobrov <a.bobrov@tinkoff.ru>
2020-02-10 04:42:58 +0300

    add deploy script

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const urls =
   process.env.NODE_ENV === 'production'
     ? {
-        catsApi: 'http://meowle.testops.ru:3001',
+        catsApi: 'http://bobrovartem.ru/cats/api',
       }
     : {
         catsApi: 'http://localhost:3001',
