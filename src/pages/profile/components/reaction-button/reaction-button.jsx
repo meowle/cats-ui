3c026ab053423ccb19c1ec9a5f2482916c8d33bb
Artem Bobrov <meriddos@gmail.com>
2020-02-16 20:51:00 +0300

    Добавление отправки и сохранения лайков/дизлайков

--- src/pages/profile/components/reaction-button/reaction-button.jsx
+++ src/pages/profile/components/reaction-button/reaction-button.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { CatsApi } from '../../../../api/cats';
+import classNames from 'classnames';
+import { ReactionApi } from '../../../../api/reaction';
+import { storage } from '../../../../utils/storage';
+import { notify } from '../../../../utils/notifications/notifications';
+import styles from './reaction-button.module.css';
 
 const emojiMap = {
   like: '👍',
@@ -10,29 +14,66 @@ const titleMap = {
   like: 'Лайкнуть',
   dislike: 'Убрать лайк',
 };
+const countField = {
+  like: 'likes',
+  dislike: 'dislikes',
+};
 
-export function ReactionButton({
-  catId,
-  isDisabled,
-  type = 'like',
-  count = 0,
-}) {
+export function ReactionButton({ catInfo, type = 'like', updateCatInfo }) {
+  const storageMethod = {
+    like: storage.likes,
+    dislike: storage.dislikes,
+  }[type];
+  const [isReacted, updateReacted] = useState(storageMethod.exist(catInfo.id));
+  const [isLoading, setLoading] = useState(false);
+  const styleReacted =
+    isReacted && !isLoading ? styles['is-reacted'] : 'is-light';
+
+  const onClick = () => {
+    setLoading(true);
+    sendReaction(type, catInfo, isReacted)
+      .then(() => {
+        const storMethod = [storageMethod.set, storageMethod.remove][
+          Number(isReacted)
+        ];
+
+        storMethod(catInfo.id);
+        updateReacted(!isReacted);
+        updateCatInfo({
+          [countField[type]]: catInfo[countField[type]] + (isReacted ? -1 : 1),
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
     <button
-      class="button is-light"
+      className={classNames('button', styles['reaction-button'], styleReacted, {
+        'is-loading': isLoading,
+      })}
       type="button"
       title={titleMap[type]}
-      onClick={onClick.bind(tpe)}
+      onClick={onClick}
     >
-      {emojiMap[type]}&nbsp;<span>{count}</span>
+      {emojiMap[type]}&nbsp;<span>{catInfo[countField[type]]}</span>
     </button>
   );
 }
 ReactionButton.propTypes = {
-  catId: PropTypes.number.isRequired,
-  isDisabled: PropTypes.bool,
+  catInfo: PropTypes.object.isRequired,
   type: PropTypes.oneOf(['like', 'dislike']).isRequired,
-  count: PropTypes.number.isRequired,
+  updateCatInfo: PropTypes.func.isRequired,
 };
 
-function onClick(type) {}
+function sendReaction(type, catInfo, isReacted) {
+  const apiMethod = {
+    like: [ReactionApi.like, ReactionApi.removeLike],
+    dislike: [ReactionApi.dislike, ReactionApi.removeDislike],
+  }[type][isReacted ? 1 : 0];
+
+  return apiMethod(catInfo.id).catch(message => {
+    notify.error(message);
+  });
+}
